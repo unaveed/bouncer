4af0bca4401f667ea1b964f8dd845a13dd6d924d
Greg Anderson <greg@infobytes.com>
2014-03-13 16:16:25 -0600

    Started parsing command line arguments

--- bouncer.c
+++ bouncer.c
@@ -1,5 +1,40 @@
 /* Written by Greg Anderson and Umair Naveed */
 
-int main() {
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+	char ext[4] = {0};
+	char correctExt[4] = {"jpg"};
+	char *input_file;
+	const char *dot;
+
+	if (argc > 1)
+		printf("argv[1]=%d\n", strlen(argv[1]));
+	else {
+		printf("No arguments supplied.\n");
+		return 1;
+	}
+
+	input_file = argv[1];	/* Set input file */
+	
+	dot = strrchr(input_file, '.');
+
+    if(!dot || dot == input_file) {
+		printf("Invalid file.\n");
+		return 1;
+	}
+
+    printf("ext=%s\n", dot + 1);
+
+	//strncpy(ext, input_file+dot+1, strlen(input_file)-dot+1);
+
+	if (strncmp(ext, correctExt, 4)) {
+		printf("Correct!\n");
+		printf("ext=%s, correctExt=%s\n", ext, correctExt);
+	}
+	else
+		printf("WRONG!!!\n");
+
 	return 0;
 }
