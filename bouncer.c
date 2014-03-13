2a7fda80471dd306594d941fd818c543df04a69c
Greg Anderson <greg@infobytes.com>
2014-03-13 17:14:22 -0600

    Fixed extension checking, now works

--- bouncer.c
+++ bouncer.c
@@ -4,10 +4,10 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
-	char ext[4] = {0};
 	char correctExt[4] = {"jpg"};
 	char *input_file;
 	const char *ext;
+	int result;
 
 	if (argc > 1)
 		printf("argv[1]=%d\n", strlen(argv[1]));
@@ -27,9 +27,8 @@ int main(int argc, char **argv) {
 
     printf("ext=%s, correctExt=%s\n", ext + 1, correctExt);
 
-	if (strncmp(ext+1, correctExt, 4)) {
+	if (!strncmp(ext+1, correctExt, 4))
 		printf("Correct!\n");
-	}
 	else
 		printf("WRONG!!!\n");
 
