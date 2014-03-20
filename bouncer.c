72993216e99973df43c0ed12ead397e9b78fbb47
Greg Anderson <greg@infobytes.com>
2014-03-20 17:11:39 -0600

    Added function for encoding image, though it is broken

--- bouncer.c
+++ bouncer.c
@@ -8,6 +8,120 @@
 
 
 
+
+
+/*
+ * Video encoding example
+ */
+static void video_encode_example(const char *filename, int codec_id, int width, int height)
+{
+    AVCodec *codec;
+    AVCodecContext *c= NULL;
+    int i, ret, x, y, got_output;
+    FILE *f;
+    AVFrame *frame;
+    AVPacket pkt;
+    uint8_t endcode[] = { 0, 0, 1, 0xb7 };
+
+    printf("Encode video file %s\n", filename);
+
+    /* find the mpeg1 video encoder */
+    codec = avcodec_find_encoder(codec_id);
+    if (!codec) {
+        fprintf(stderr, "Codec not found\n");
+        exit(1);
+    }
+
+    c = avcodec_alloc_context3(codec);
+    if (!c) {
+        fprintf(stderr, "Could not allocate video codec context\n");
+        exit(1);
+    }
+
+    /* resolution must be a multiple of two */
+    c->width = width;
+    c->height = height;
+    c->max_b_frames = 1;
+    c->pix_fmt = codec->pix_fmts[0];
+
+
+    if (codec_id == AV_CODEC_ID_H264)
+        av_opt_set(c->priv_data, "preset", "slow", 0);
+
+    /* open it */
+    if (avcodec_open2(c, codec, NULL) < 0) {
+        fprintf(stderr, "Could not open codec\n");
+        exit(1);
+    }
+
+    f = fopen(filename, "wb");
+    if (!f) {
+        fprintf(stderr, "Could not open %s\n", filename);
+        exit(1);
+    }
+
+    frame = av_frame_alloc();
+    if (!frame) {
+        fprintf(stderr, "Could not allocate video frame\n");
+        exit(1);
+    }
+    frame->format = c->pix_fmt;
+    frame->width  = c->width;
+    frame->height = c->height;
+
+    /* the image can be allocated by any means and av_image_alloc() is
+     * just the most convenient way if av_malloc() is to be used */
+    ret = av_image_alloc(frame->data, frame->linesize, c->width, c->height, c->pix_fmt, 32);
+
+    if (ret < 0) {
+        fprintf(stderr, "Could not allocate raw picture buffer\n");
+        exit(1);
+    }
+
+    /* encode 1 second of video */
+	av_init_packet(&pkt);
+	pkt.data = NULL;    // packet data will be allocated by the encoder
+	pkt.size = 0;
+
+
+	/* encode the image */
+	ret = avcodec_encode_video2(c, &pkt, frame, &got_output);
+	if (ret < 0) {
+		fprintf(stderr, "Error encoding frame\n");
+		exit(1);
+	}
+
+	if (got_output) {
+		printf("Write frame %3d (size=%5d)\n", i, pkt.size);
+		fwrite(pkt.data, 1, pkt.size, f);
+		av_free_packet(&pkt);
+	}
+
+    /* add sequence end code to have a real mpeg file */
+    fwrite(endcode, 1, sizeof(endcode), f);
+    fclose(f);
+
+    avcodec_close(c);
+    av_free(c);
+    av_freep(&frame->data[0]);
+    av_frame_free(&frame);
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
  * Fills the first supplied row with the correct pixel data to contain the circle.
  */
@@ -102,17 +216,12 @@ void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame, int centerX, 
 		/* Normal picture */
 		else
 			fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
-
-		/*
-		if (y % 2 == 0)
-			fwrite(green, 1, width*3, pFile);
-		else
-			fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
-		*/
 	}
 
 	// Close file
 	fclose(pFile);
+
+	video_encode_example("frame000.ppm", AV_CODEC_ID_XKCD, width, height);
 }
 
 int main(int argc, char *argv[]) {
